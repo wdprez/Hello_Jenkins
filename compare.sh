354c8e9685953288e7b6b8fcdfc7ce7237087cb3
wdprez <wouter.deprez@telenet.be>
2018-12-07 10:45:39 +0100

    Update compare.sh

--- compare.sh
+++ compare.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 BR=$(git branch)
 echo "$BR"
-DIFF=$(git diff origin master)
-if [ "$DIFF" == "" ]; then
-        echo "equal";
-else
-        echo "not equal";
-fi
+#DIFF=$(git diff origin master)
+#if [ "$DIFF" == "" ]; then
+#        echo "equal";
+#else
+#        echo "not equal";
+#fi
